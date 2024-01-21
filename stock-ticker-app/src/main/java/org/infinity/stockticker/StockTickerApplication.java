d8dc1c0e3efea1de9424662866a8c9857a425d45
Avinash Ghodke <avi.ghodke01@gmail.com>
2024-01-21 18:15:05 +0530

    Initial commit

---
+++ stock-ticker-app/src/main/java/org/infinity/stockticker/StockTickerApplication.java
@@ -0,0 +1,13 @@
+package org.infinity.stockticker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockTickerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockTickerApplication.class, args);
+	}
+
+}
---
+++ stock-ticker-app/src/test/java/org/infinity/stockticker/StockTickerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.infinity.stockticker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockTickerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
